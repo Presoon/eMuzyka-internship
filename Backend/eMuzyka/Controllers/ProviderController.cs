90374f564db9c219057564ec6c1a085e46ac166e
Kamil Szpak <kamilszpak99@gmail.com>
2021-04-18 18:30:47 +0200

    Add Privider controller

---
+++ Backend/eMuzyka/Controllers/ProviderController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using eMuzyka.Database;
+using eMuzyka.Entities;
+using Microsoft.EntityFrameworkCore.Scaffolding.Metadata;
+
+namespace eMuzyka.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProviderController : ControllerBase
+    {
+        private readonly DatabaseContext _dbContext;
+
+        public ProviderController(DatabaseContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        [Route("all")]
+        public ActionResult<IEnumerable<Provider>> GetAll()
+        {
+            var providers = _dbContext.Providers.ToList();
+            return Ok(providers);
+        }
+    }
+}
