420dbf9e78d9ea82d82f7f56d2d33d35ba7f62f1
Kamil Szpak <kamilszpak99@gmail.com>
2021-04-18 13:56:27 +0200

    Add entities in Database model

---
+++ Backend/eMuzyka/Entities/Album.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using System.Threading.Tasks;
+
+namespace eMuzyka.Entities
+{
+    public class Album
+    {
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string ArtistName { get; set; }
+        public string Version { get; set; }
+        public DateTime ReleaseDate { get; set; }
+
+
+        public int ProviderId { get; set; }
+        public virtual Provider Provider { get; set; }
+        
+
+        public virtual List<Track> Tracks { get; set; }
+        
+    }
+}
---
+++ Backend/eMuzyka/Entities/Track.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eMuzyka.Entities
+{
+    public class Track
+    {
+        public int Id { get; set; }
+        public string ArtistName { get; set; }
+        public string Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public int Duration { get; set; }
+
+
+        public int AlbumId { get; set; }
+        public virtual Album Album { get; set; }
+    }
+}
