feb6117cf678407f8a63ea190a282c9eecdcc78d
Kamil Szpak <kamilszpak99@gmail.com>
2021-04-18 14:16:16 +0200

    Prepare DbContext for adding migration

---
+++ Backend/eMuzyka/Database/DbContext.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using eMuzyka.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace eMuzyka.Database
+{
+    public class DbContext : Microsoft.EntityFrameworkCore.DbContext
+    {
+        private readonly IConfiguration _configuration;
+        private readonly string _connectionString;
+
+        public DbContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _connectionString = _configuration.GetConnectionString("connStringAzure");
+        }
+
+        public DbSet<Provider> Providers { get; set; }
+        public DbSet<Album> Albums { get; set; }
+        public DbSet<Track> Tracks { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Provider>()
+                .Property(r => r.UserName)
+                .IsRequired()
+                .HasMaxLength(20);
+            modelBuilder.Entity<Provider>()
+                .Property(r => r.Password)
+                .IsRequired();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_connectionString);
+        }
+    }
+}
