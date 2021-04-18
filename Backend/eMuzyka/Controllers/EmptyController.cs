b054bc740e8146b2543292e8d25515e5244fec64
Kamil Szpak <kamilszpak99@gmail.com>
2021-04-18 13:36:10 +0200

    Create an empty .NET Core WebAPI project

---
+++ Backend/eMuzyka/Controllers/EmptyController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eMuzyka.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EmptyController : ControllerBase
+    {
+
+        [HttpGet]
+        public void Get()
+        {
+            return;
+        }
+    }
+}
