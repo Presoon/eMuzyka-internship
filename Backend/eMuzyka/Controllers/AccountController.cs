8030def55e3455766495182f0ab6b1da1da983f6
Kamil Szpak <kamilszpak99@gmail.com>
2021-04-19 18:42:39 +0200

    Implement basic Account controller

---
+++ Backend/eMuzyka/Controllers/AccountController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using eMuzyka.DTO.Provider;
+using eMuzyka.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eMuzyka.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class AccountController : ControllerBase
+    {
+        private readonly IAccountService _accountService;
+
+        public AccountController(IAccountService accountService)
+        {
+            _accountService = accountService;
+        }
+
+        [HttpPost]
+        [Route("register")]
+        public ActionResult RegisterProvider([FromBody] RegisterProviderDto registerProvider)
+        {
+
+            _accountService.RegisterProvider(registerProvider);
+            return Ok();
+        }
+
+    }
+}
