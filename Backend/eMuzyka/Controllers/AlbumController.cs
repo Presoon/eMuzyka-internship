2e3a1bdd2a4b83607c176d5ba5558e282516c249
Kamil Szpak <kamilszpak99@gmail.com>
2021-04-19 10:33:23 +0200

    Implement Album controller

---
+++ Backend/eMuzyka/Controllers/AlbumController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+using eMuzyka.Database;
+using eMuzyka.DTO.Album;
+using eMuzyka.DTO.Provider;
+using eMuzyka.Entities;
+using eMuzyka.Services;
+
+
+namespace eMuzyka.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AlbumController : ControllerBase
+    {
+        private readonly IAlbumService _albumService;
+
+        public AlbumController(IAlbumService albumService)
+        {
+            _albumService = albumService;
+        }
+
+
+        [HttpGet]
+        [Route("{id}/all")]
+        public ActionResult<IEnumerable<AlbumDto>> GetAllByProviderId([FromRoute] int id)
+        {
+            var result = _albumService.GetAllByProviderId(id);
+            return Ok(result);
+        }
+
+
+        [HttpGet]
+        [Route("{id}")]
+        public ActionResult<AlbumDto> GetById([FromRoute]int id)
+        {
+            var result = _albumService.GetById(id);
+            return Ok(result);
+        }
+
+
+        [HttpGet]
+        [Route("{id}/tracks")]
+        public ActionResult<AlbumWTracksDto> GetWithTracksById([FromRoute] int id)
+        {
+            var result = _albumService.GetWithTracksById(id);
+            return Ok(result);
+        }
+    }
+}
