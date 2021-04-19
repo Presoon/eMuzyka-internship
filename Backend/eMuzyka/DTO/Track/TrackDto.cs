2e1ebf5438aef091faf154f8b22b9b981b6e2947
Kamil Szpak <kamilszpak99@gmail.com>
2021-04-19 10:31:59 +0200

    Add Track DTO class

---
+++ Backend/eMuzyka/DTO/Track/TrackDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eMuzyka.DTO.Track
+{
+    public class TrackDto
+    {
+        public int Id { get; set; }
+        public int TrackNumber { get; set; }
+        public string ArtistName { get; set; }
+        public string Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public int Duration { get; set; }
+    }
+}
